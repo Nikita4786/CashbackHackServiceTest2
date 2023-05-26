53e92c84685c0e80af59b27257c9f2460c2b2eec
Nikita <grigoryantsnm@gmail.com>
2023-05-26 22:25:09 +0300

    new test commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,28 +5,38 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
-    @Test
+    @org.testng.annotations.Test
     void shouldCalculateMissingQty() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     void amountEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int expected = 500;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
-    @Test
-    void amountSoreBoundary () {
+
+    @org.testng.annotations.Test
+    void amountSoreBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testShouldAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
         assertEquals(actual,expected);
     }
